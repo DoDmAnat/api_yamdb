35d048fefc21e1bf9e1d553ac0f043c772a499d5
Надим Шакиржанов~ <aversul@gmail.com>
2022-11-01 19:23:47 +0400

    тест слияния 3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,9 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
-    'reviews',
     'api',
+    'reviews',
+    'users',
 ]
 
 MIDDLEWARE = [
