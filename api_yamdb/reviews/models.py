2914f7f3471aae453749c6ca3d519e79e25a16b3
Надим Шакиржанов~ <aversul@gmail.com>
2022-11-01 19:21:12 +0400

    слияние с мастером

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,46 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
-
-
-class User(AbstractUser):
-    ADMIN = 'admin'
-    MODERATOR = 'moderator'
-    USER = 'user'
-    ROLES = [(ADMIN, 'Administrator'),
-             (MODERATOR, 'Moderator'),
-             (USER, 'User'), ]
-
-    username = models.CharField(verbose_name='Имя пользователя',
-                                max_length=150,
-                                null=True,
-                                unique=True)
-    email = models.EmailField(verbose_name='Адрес почты',
-                              unique=True,)
-    role = models.CharField(verbose_name='Должность',
-                            max_length=50,
-                            choices=ROLES,
-                            default=USER)
-    bio = models.TextField(verbose_name='Биография',
-                           null=True,
-                           blank=True)
-
-    def is_moderator(self):
-        return self.role == self.MODERATOR
-
-    def is_admin(self):
-        return self.role == self.ADMIN
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        # Не уверен что правильно, но как-то так
-        constraints = [
-            models.CheckConstraint(check=models.Q(username='me'),
-                                   name='username_is_not_me')]
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -82,8 +44,7 @@ class Title(models.Model):
                                    null=True,
                                    blank=True)
     genre = models.ManyToManyField(Genre,
-                                   verbose_name='Жанр произведения',
-                                   through='GenreTitle')
+                                   verbose_name='Жанр произведения')
     category = models.ForeignKey(Category,
                                  verbose_name='Категория произведения',
                                  on_delete=models.SET_NULL,
@@ -107,7 +68,7 @@ class Review(models.Model):
                               verbose_name='Произведение',
                               on_delete=models.CASCADE,
                               related_name='reviews')
-    text = models.TextField(verbose_name='Текст',)
+    text = models.TextField(verbose_name='Текст', )
     author = models.ForeignKey(User,
                                verbose_name='Автор',
                                on_delete=models.CASCADE,
@@ -137,7 +98,7 @@ class Comment(models.Model):
                                verbose_name='Отзыв',
                                on_delete=models.CASCADE,
                                related_name='comments')
-    text = models.TextField(verbose_name='Текст',)
+    text = models.TextField(verbose_name='Текст', )
     author = models.ForeignKey(User,
                                verbose_name='Пользователь',
                                on_delete=models.CASCADE,
