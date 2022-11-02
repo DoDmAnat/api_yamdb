c051680d99073f208f513796b1503caf3881e4a7
Надим Шакиржанов~ <aversul@gmail.com>
2022-11-02 22:47:20 +0400

    сделал слияние с мастером, закомитил несколько строк в urls т.к. не давали запуститься серверу

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -18,13 +18,12 @@ v1_router = DefaultRouter()
 v1_router.register('titles', TitleViewSet)
 v1_router.register('genres', GenreViewSet)
 v1_router.register('categories', CategoryViewSet)
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews')
-v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments'
-)
+# v1_router.register(
+#    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews')
+# v1_router.register(
+#    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+#    CommentViewSet,
+#    basename='comments')
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
